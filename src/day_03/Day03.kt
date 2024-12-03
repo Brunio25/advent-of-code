d753cb8089e73a955c2a4d95d6a8ec4d3334656a
Bruno Soares <brunofpsoares25@gmail.com>
2024-12-03 13:58:11 +0000

    day 3 - 2024

---
+++ src/day_03/Day03.kt
@@ -0,0 +1,29 @@
+package day_03
+
+import Day
+
+class Day03 : Day() {
+    private val input by lazy { readFileList.reduce { acc, s -> acc + s } }
+
+    private val mulRegex = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
+
+    private fun String.part1Compute(): Int = mulRegex.findAll(this)
+        .sumOf { it.destructured.let { (l, r) -> l.toInt() * r.toInt() } }
+
+    override fun part1(): Int = input.part1Compute()
+
+    override fun part2(): Int = input
+        .remove(dontToDo)
+        .part1Compute()
+
+    private val dontToDo = """(don't\(\).*?(?=do\(\)|$))""".toRegex()
+
+    private fun String.remove(regex: Regex): String = replace(regex, "")
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Day03().run()
+        }
+    }
+}
