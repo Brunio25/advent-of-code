25b9c14649a28bd91f8c854d53e788d80638e1ec
Bruno Soares <brunofpsoares25@gmail.com>
2024-12-08 23:46:15 +0000

    day 8 - 2024

--- src/main/kotlin/helpers/Coordinates.kt
+++ src/main/kotlin/helpers/Coordinates.kt
@@ -1,3 +1,25 @@
 package helpers
 
-data class Coordinates2D(val x: Int, val y: Int)
+import kotlin.math.atan
+import kotlin.math.cos
+import kotlin.math.roundToInt
+import kotlin.math.sin
+
+data class Coordinates2D(val x: Int, val y: Int) {
+    operator fun plus(vector: Vector): Coordinates2D = Coordinates2D(
+        x = (x + vector.magnitude * cos(vector.angle)).roundToInt(),
+        y = (y + vector.magnitude * sin(vector.angle)).roundToInt()
+    )
+
+    fun vector(other: Coordinates2D): Vector {
+        val xDelta = (other.x - x).toDouble()
+        val yDelta = (other.y - y).toDouble()
+
+        val angle = atan(yDelta / xDelta)
+        val hypotenuse = yDelta / sin(angle)
+
+        return Vector(angle, hypotenuse)
+    }
+}
+
+data class Vector(val angle: Double, val magnitude: Double)
